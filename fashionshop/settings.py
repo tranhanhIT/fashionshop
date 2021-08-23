518d863714ae5dc2663c6dc1d89d60480b69be28
tranhanhIT <vincentvn2005@gmail.com>
2021-08-23 21:57:53 +0200

    The third time: django authentication

--- fashionshop/settings.py
+++ fashionshop/settings.py
@@ -138,3 +138,15 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP configuration 587
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'macheriehong@gmail.com'
+EMAIL_HOST_PASSWORD = 'COgang2019!'
+EMAIL_PORT = 587
